ec02d9c3c5b27f954e1502b0116c03501938818e
Ashaba Derrick <derrick@victorium.biz>
2022-12-06 17:04:10 +0300

    my_gallery_screen with permision check button

--- gallery/lib/my_gallery_screen.dart
+++ gallery/lib/my_gallery_screen.dart
@@ -1,12 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:gallery/gallery_screen.dart';
+import 'package:gallery/utils.dart';
+import 'package:photo_manager/photo_manager.dart';
 
-class MyGalleryScreen extends StatelessWidget {
+class MyGalleryScreen extends StatefulWidget {
   const MyGalleryScreen({super.key});
 
   @override
+  State<MyGalleryScreen> createState() => _MyGalleryScreenState();
+}
+
+class _MyGalleryScreenState extends State<MyGalleryScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Gallery")),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: whitecolor,
+        centerTitle: true,
+        title: const Text(
+          "Gallery",
+          style: TextStyle(color: blackcolor),
+        ),
+        iconTheme: const IconThemeData(color: blackcolor),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: ElevatedButton(
+            // make the function async
+            onPressed: () async {
+              // ### Add the next 2 lines ###
+              final permisionState =
+                  await PhotoManager.requestPermissionExtend();
+              if (!permisionState.hasAccess) return;
+
+              // ignore: use_build_context_synchronously
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (_) => Gallery()),
+              );
+            },
+            child: const Text('Open Gallery'),
+          ),
+        ),
+      ),
     );
   }
 }
